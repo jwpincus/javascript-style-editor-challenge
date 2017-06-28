ab9262fa6331be0996a529e55486a46db6f7bc0f
Jack Pincus <jwpincus@gmail.com>
2017-06-28 08:25:55 -0600

    first commit

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-  // your code goes here.
+//   retrieving the values from the form,
+  $("#style_editor").submit(function(){
+    event.preventDefault();
+    var selector = $(this).find('input[name="selector"]').val();
+    var property = $(this).find('input[name="property"]').val();
+    var value = $(this).find('input[name="value"]').val();
+  });
+//    passing those values into the correct jQuery function for manipulating CSS, and
+  $(selector)
+//    binding the previous two parts to a particular event using an event handler.
+
 });
